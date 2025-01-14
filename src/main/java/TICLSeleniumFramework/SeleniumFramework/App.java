953ba3f40cc3a8e30ddc1e15671f3709353c0086
shivanandkoti <shivanandb.koti@ust.com>
2025-01-14 13:19:18 +0530

    Update App.java
    Updated

--- src/main/java/TICLSeleniumFramework/SeleniumFramework/App.java
+++ src/main/java/TICLSeleniumFramework/SeleniumFramework/App.java
@@ -5,6 +5,6 @@ package TICLSeleniumFramework.SeleniumFramework;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("My World!");
     }
 }
